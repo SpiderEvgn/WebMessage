b3929f9f3995623510f562136c75b0b2e8deae01
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-12-30 11:00:28 +0800

    add 404 routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'contacts#index'
+
+  match '*a', to: 'errors#routing', via: :all
 end
