151fa21f6f0722147cade0b862b5067df9bf975f
SpiderEvgn <xiajun.zhang@usingnow.com>
2017-01-03 14:19:02 +0800

    修改登录名显示

--- app/channels/contacts_channel.rb
+++ app/channels/contacts_channel.rb
@@ -2,7 +2,7 @@
 class ContactsChannel < ApplicationCable::Channel
   # Called when the consumer has successfully become a subscriber of this channel.
   def subscribed
-     stream_from "contacts_list_#{params['current_user_id']}_channel"
+    stream_from "contacts_list_#{params['current_user_id']}_channel"
   end
 
   def unsubscribed
